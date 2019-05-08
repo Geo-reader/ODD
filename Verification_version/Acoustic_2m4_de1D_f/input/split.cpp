9fe0eadcfceda175076efc57dbe4485486537809
NING WANG <wangn2311@163.com>
2019-05-08 00:35:30 -0400

    Update split.cpp

--- Verification_version/Acoustic_2m4_de1D_f/input/split.cpp
+++ Verification_version/Acoustic_2m4_de1D_f/input/split.cpp
@@ -1,28 +1,64 @@
+/* 
+This progranm is designed to split the whole velocity and density models into several sub-domains
+
+Coordinate configuration of seismic data:
+ is=iz*ny*nx+iy*nx+ix;
+ The fastest dim:        *.x
+ The second fastest dim: *.y
+ The slowest dim:        *.z
+ 
+ Acknowledgement:
+
+   Copyright (C) 2019 China University of Petroleum, Beijing
+   Copyright (C) 2019 Ning Wang
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details: http://www.gnu.org/licenses/
+
+*/
+
 #include"stdio.h"
 #include"stdlib.h"
 #include <string.h>
-#define pml 15
-#define nz 512
-#define ny 512
-#define nx 512
-#define numpx 16
-
+#define pml 15            //the number of PML layers
+#define nz 512            // z- grid number of model(without PML)
+#define ny 512            // y- grid number of model(without PML)
+#define nx 512            // x- grid number of model(without PML)
+#define numpx 16          //the number of sub-domains at x- direction
+#define numpy 1           //the number of sub-domains at y- direction
 
 void num2str(char asc[6],int num);
 
 int main()
 {
-    int sizex[numpx];
-    int xef[numpx];
-    int modx,avex;
+    int sizex[numpx]; //arrays for storing the number of grid points (with pml) for sub-domains at x- direction
+    int xef[numpx];   //arrays for storing the number of grid points (without pml) for sub-domains at x- direction
+	int sizey[numpy]; //arrays for storing the number of grid points (with pml) for sub-domains at y- direction
+    int yef[numpy];   //arrays for storing the number of grid points (without pml) for sub-domains at y- direction
+    int modx;         //the remainder of the number of whole grid points at x- axis(with pml) divided by the number of sub-domains at x- direction
+	int avex;    
+    int mody;         //the remainder of the number of whole grid points at y- axis(with pml) divided by the number of sub-domains at y- direction
+	int	avey;
 
-
-    int nxp=nx+2*pml;
+    int nxp=nx+2*pml;// x- grid number of model(with PML)
     modx=nxp%numpx;
     avex=(nxp-modx)/numpx;
 
+    int nyp=ny+2*pml;// y- grid number of model(with PML)
+    mody=nyp%numpy;
+    avey=(nyp-mody)/numpy;
 
-    int i,j,k;
+    int ix,iy,iz,i,j,is;
+    int offset_y,offset_x;
+
+	//define the number of grid-points for every sub-domain at x- direction 
     for(i=0;i<numpx;i++)
     {
         sizex[i]=avex;
@@ -35,102 +71,154 @@ int main()
         printf("i=%d size=%d\n",i,xef[i]);
 
     }
-
-    FILE *fp=fopen("rho512.bin","rb");
-    FILE *fp1;
-    char name[40],ascx[6],ascy[6];
-	float *yoz;
-
-    FILE *fpv=fopen("vp512.bin","rb");
-    FILE *fpv1;
-    char namev[40];
-    float *yozv;
-   
-    
-    for(i=0;i<numpx;i++)
+	//define the number of grid-points for every sub-domain at y- direction 
+    for(j=0;j<numpy;j++)
     {
-	    yoz=(float*)malloc(sizeof(float)*ny*nz);
-		yozv=(float*)malloc(sizeof(float)*ny*nz);
+        sizey[j]=avey;
+        if(j<mody){sizey[j]=sizey[j]+1;}
 
-        num2str(ascx,i);
-        strcpy(name,"");
-        sprintf(name,"x%s-rho.dat",ascx);
-        fp1=fopen(name,"wb");
+        yef[j]=sizey[j];
+        if(j==0){yef[j]=yef[j]-pml;}
+        if(j==numpy-1){yef[j]=yef[j]-pml;}
 
-        num2str(ascx,i);
-        strcpy(namev,"");
-        sprintf(namev,"x%s-vp.dat",ascx);
-        fpv1=fopen(namev,"wb");
+      printf("j=%d size=%d\n",j,yef[j]);
 
-        k=0;
-        do{
-           fread(yoz,sizeof(float),ny*nz,fp);
-           fwrite(yoz,sizeof(float),ny*nz,fp1);
-           fread(yozv,sizeof(float),ny*nz,fpv);
-           fwrite(yozv,sizeof(float),ny*nz,fpv1);
+    }
 
-           k=k+1;
-          }while(k<xef[i]);
-         fclose(fp1);
-		 fclose(fpv1);
-		 free(yoz);
-		 free(yozv);
-	}
-    fclose(fp);fclose(fpv);
-    
-    
+   float *vp_whole;  // the whole velocity model
+   float *vp_each;   // velociy models for sub-domains
+   FILE *fp=fopen("vp512.bin","rb");
+   FILE *fpp1[numpy][numpx];
+   char name[60],ascx[6],ascy[6];
+   vp_whole=(float*)malloc(sizeof(float)*nx*ny*nz);
+
+   float *den_whole;  // the whole density model
+   float *den_each;   // density models for sub-domains
+   FILE *fp2=fopen("rho512.bin","rb");
+   FILE *fpp2[numpy][numpx];
+   char name2[60];
+   den_whole=(float*)malloc(sizeof(float)*nx*ny*nz);
+
+      //reading the whole velocity & density models
+	for(iz=0;iz<nz;iz++)
+	{
+		for(iy=0;iy<ny;iy++)
+		{
+			for(ix=0;ix<nx;ix++)
+			{
+			is=iz*ny*nx+iy*nx+ix;
+			fread(&vp_whole[is],sizeof(float),1,fp);
+			fread(&den_whole[is],sizeof(float),1,fp2);
+			}
+		}
+	}	
+	 //spliting the whole velocity & density models into subdomains    
+    offset_y=0;
+        for(j=0;j<numpy;j++)
+        {
+         num2str(ascy,j);                  
+         
+		 offset_x=0;
+		for(i=0;i<numpx;i++)
+        {
+         num2str(ascx,i);          
+          sprintf(name,"x%s_vp_y%s.dat",ascx,ascy);
+          fpp1[j][i]=fopen(name,"wb"); 
+		  vp_each=(float*)malloc(sizeof(float)*xef[i]*yef[j]*nz);
+
+          sprintf(name2,"x%s_rho_y%s.dat",ascx,ascy);
+          fpp2[j][i]=fopen(name2,"wb"); 
+		  den_each=(float*)malloc(sizeof(float)*xef[i]*yef[j]*nz);
+		 
+         for(iz=0;iz<nz;iz++)
+         
+	       {
+		    for(iy=0;iy<yef[j];iy++)
+		      {
+		        for(ix=0;ix<xef[i];ix++)
+		        {
+			       is=iz*yef[j]*xef[i]+iy*xef[i]+ix;
+				   vp_each[is]=vp_whole[iz*ny*nx+(iy+offset_y)*nx+(ix+offset_x)];
+			       fwrite(&vp_each[is],sizeof(float),1,fpp1[j][i]);
+
+				   den_each[is]=den_whole[iz*ny*nx+(iy+offset_y)*nx+(ix+offset_x)];
+			       fwrite(&den_each[is],sizeof(float),1,fpp2[j][i]);
+			    }
+		     }  
+		   }
+
+			offset_x=offset_x+xef[i];
+		 } 
+		 offset_y=offset_y+yef[j];  
+		}
+
+        for(j=0;j<numpy;j++)
+        {
+
+		    for(i=0;i<numpx;i++)
+              {
+			   fclose(fpp1[j][i]);
+			   fclose(fpp2[j][i]);
+		      }
+		}
+  
+    fclose(fp);fclose(fp2);
+    free(vp_whole);free(vp_each);free(den_whole);free(den_each);    
+
     return 0;
 }
-
+// ==========================================================
+//  This subroutine is used for transfroming number to string
+//  =========================================================
 void num2str(char asc[6],int num)
 {
   char asc1,asc2,asc3,asc4,asc5,asc6;
   asc6='\0';
   if(num<10)
   {
-	asc5='0'; // wan
-    asc4='0'; // qian
-    asc3='0'; // bai
-    asc2='0'; // shi
-    asc1=(char)(num+48); // ge
+	asc5='0'; 
+    asc4='0'; 
+    asc3='0'; 
+    asc2='0'; 
+    asc1=(char)(num+48); 
   }
   if(num>=10&&num<=99)
   {
-    asc5='0'; // wan
-    asc4='0'; // qian
-    asc3='0'; // bai
-    asc2=(char)(num/10+48); // shi
-    asc1=(char)(num-num/10*10+48); // ge
+    asc5='0'; 
+    asc4='0'; 
+    asc3='0'; 
+    asc2=(char)(num/10+48); 
+    asc1=(char)(num-num/10*10+48); 
   }
   if(num>99&&num<=999)
   {
-	asc5='0';                    // wan
-    asc4='0';                    // qian
-    asc3=(char)(num/100+48);     //bai
-    asc2=(char)(num%100/10+48);  //shi
-    asc1=(char)(num%10+48);      //ge
+	asc5='0';                    
+    asc4='0';                   
+    asc3=(char)(num/100+48);     
+    asc2=(char)(num%100/10+48);  
+    asc1=(char)(num%10+48);      
   }
   if(num>=1000&&num<=9999)
   {
-	asc5='0'; // wan
-    asc4=(char)(num/1000+48);      //qian
-    asc3=(char)((num/100)%10+48); //bai
-    asc2=(char)((num/10)%10+48);  //shi
-    asc1=(char)(num%10+48);       //ge
+	asc5='0'; 
+    asc4=(char)(num/1000+48);      
+    asc3=(char)((num/100)%10+48);
+    asc2=(char)((num/10)%10+48);  
+    asc1=(char)(num%10+48);       
 
   }
   if(num>=10000&&num<=99999)
   {
-	  asc5=(char)(num/10000+48); // wan
-      asc4=(char)((num/1000)%10+48); //qian
-      asc3=(char)((num/100)%10+48);//bai
-      asc2=(char)((num/10)%10+48); //shi
-      asc1=(char)(num%10+48);        //ge
+	  asc5=(char)(num/10000+48); 
+      asc4=(char)((num/1000)%10+48); 
+      asc3=(char)((num/100)%10+48);
+      asc2=(char)((num/10)%10+48); 
+      asc1=(char)(num%10+48);        
 
   }
-   asc[0]=asc5;  // bai
-   asc[1]=asc4;  // shi
-   asc[2]=asc3;  // ge
+   asc[0]=asc5;  
+   asc[1]=asc4;  
+   asc[2]=asc3;  
    asc[3]=asc2;
    asc[4]=asc1;
    asc[5]=asc6;
