8f488050dc424a3dab4a4db2674bca5444a4021b
NING WANG <wangn2311@163.com>
2019-05-08 13:24:30 -0400

    Update splice.cpp

--- Verification_version/Acoustic_2m4_de2D/output/splice.cpp
+++ Verification_version/Acoustic_2m4_de2D/output/splice.cpp
@@ -1,3 +1,29 @@
+/* 
+This progranm is designed to split the whole velocity and density models into several sub-domains
+
+Coordinate configuration of seismic data:
+ is=iz*ny*nx+iy*nx+ix;
+ The fast dim:        *.x
+ The second fast dim: *.y
+ The slowest dim:     *.z
+ 
+ Acknowledgement:
+
+   Copyright (C) 2019 China University of Petroleum, Beijing
+   Copyright (C) 2019 Ning Wang
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details: http://www.gnu.org/licenses/
+
+*/
+
 #include"stdio.h"
 #include"stdlib.h"
 #include <string.h>
